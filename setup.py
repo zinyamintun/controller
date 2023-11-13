b4b51e712717983fc1495bcbc7be38cad6d345c7
David Gundana <david.gundana@gmail.com>
2023-11-13 16:22:11 -0500

    fix import issue

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['controller'],
-    scripts=['src/latch.py', 'src/predictor_ros.py', 'src/webcam_node.py', 'src/smooth.py', 'src/safety_switch.py', 'src/subsciber_test2.py', 'src/jackal/human_roadmap', 'src/jackal/linearization'],
-    
-    package_dir={'': 'src'}
+    packages=['linearization', 'human_roadmap'],
+    # scripts=['src/latch.py', 'src/predictor_ros.py', 'src/webcam_node.py', 'src/smooth.py', 'src/safety_switch.py', 'src/subsciber_test2.py'],
+    package_dir={'': 'src/jackal'},
 )
 
 
