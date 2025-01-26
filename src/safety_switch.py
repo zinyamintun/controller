9cdbd4cdf65fb043cd57334155d8f35a35e2d37e
zinyamintun <123512346+zinyamintun@users.noreply.github.com>
2025-01-25 18:50:36 -0800

    Update safety_switch.py

--- src/safety_switch.py
+++ src/safety_switch.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Using joystick input, LiDAR data, and velocity commands to determine safe and autonomous robot movement.
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
@@ -6,24 +7,22 @@ from sensor_msgs.msg import Joy
 from sensor_msgs.msg import LaserScan
 import math
 
-enable = False
-pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-can_go_forward = False
-can_go_backward = False
-# last_time = 0  #last time recieving bluetooth joystick message 
+# Global variables
+enable = False # Indicates whether the joystick button is pressed.
+can_go_forward = False  # Indicates whether the robot can safely move forward.
+can_go_backward = False  # Indicates whether the robot can safely move backward.
+
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10) # Publishes Twist messages to control robot velocity.
 
 def callback_listener(data):
-    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
-    
-    # diff_time = rospy.get_time() - last_time 
-    # print("current time is:  %f" % rospy.get_time())
-    # print(diff_time)
-
-    # print('heard inverted command vel')
-    if (data.linear.x > 0) :
+    """
+    Callback function for the velocity topic.
+    It checks if the robot is allowed to move based on joystick input (enable) and obstacle status (can_go_forward and can_go_backward).
+    """
+    if (data.linear.x > 0) :  # Check if the robot is commanded to move forward.
         if not enable or not can_go_forward : 
-    	    empty_twist = Twist()
-    	    pub.publish(empty_twist)
+    	    empty_twist = Twist() 
+    	    pub.publish(empty_twist) # Publish an empty Twist message to stop the robot.
         else: 
     	    pub.publish(data)
     else:
@@ -34,61 +33,66 @@ def callback_listener(data):
             pub.publish(data)
 
 def callback_joy(data):
-   # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data)
-   #  print("current joystick time is: %f" % rospy.get_time())
+    """
+    Callback function for the joystick topic.
+    Updates the 'enable' variable based on joystick button status.
+    """
     global enable
-    # global last_time
-    # last_time = rospy.get_time()
     if (data.axes[5] < 0.95 and not (data.axes[2]>-0.001 and data.axes[2]<0.001)):
     	enable = True
     else: 
     	enable = False
 
 def callback_lidar(data):
-    # min_dist = min(data.ranges)
-    # print("minimum distance is: %f" % min_dist)
+    """
+    Callback function for the LiDAR topic.
+    Updates the 'can_go_forward' and 'can_go_backward' variables based on obstacle detection.
+    """
     global can_go_forward
     global can_go_backward
+
+    # Thresholds for defining the collision distance in (meters) based on robot's velocity
     x_ThreshFront_forward = 0.75
     x_ThreshBack_forward = 0
     x_ThreshFront_backward = 0
     x_ThreshBack_backward = -0.55
     y_ThreshLeft  = 0.40
     y_ThreshRight = -0.40
+
+    # Temporary variables to determine if the path is clear.
     can_go_forward_temp = True
     can_go_backward_temp = True
+
+    # Loop through all LiDAR scan data points.
     for i in range(len(data.ranges)):
         theta = data.angle_min + (i*data.angle_increment)
         x = math.cos(theta) * data.ranges[i]
         y = math.sin(theta) * data.ranges[i]
-       # print("x coordinate is: %f" % x)
-       # print("y coordinate is: %f" % y)
+        
         isInDangerZoneX_forward = (x<x_ThreshFront_forward and x>x_ThreshBack_forward)
         isInDangerZoneX_backward = (x<x_ThreshFront_backward and x>x_ThreshBack_backward)
         isInDangerZoneY = (y<y_ThreshLeft and y>y_ThreshRight)
+        
         if isInDangerZoneX_forward and isInDangerZoneY:
             can_go_forward_temp = False
         if isInDangerZoneX_backward and isInDangerZoneY:
             can_go_backward_temp = False
+            
     can_go_forward = can_go_forward_temp
     can_go_backward = can_go_backward_temp
 
 def safety_switch():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
+    """
+    Initializes the ROS node and subscribes to relevant topics.
+    Ensures the robot's movement is controlled by joystick input and obstacle detection.
+    """
     rospy.init_node('safety_switch', anonymous=True)
-    # rospy.Subscriber("/controller/cmd_vel", Twist, callback_listener)
-    # rospy.Subscriber("/controller/cmd_vel_inverted", Twist, callback_listener)
     rospy.Subscriber("/controller/cmd_vel_smooth", Twist, callback_listener)
-
     rospy.Subscriber("/bluetooth_teleop/joy", Joy, callback_joy)
     rospy.Subscriber("/scan", LaserScan, callback_lidar)
-    # spin() simply keeps python from exiting until this node is stopped
+    # Keeps the script running until the node is manually stopped.
     rospy.spin()
 
+# Run the safety switch node when the script is executed.
 if __name__ == '__main__':
     safety_switch()
